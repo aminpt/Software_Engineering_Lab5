6cc0b73b8b9933fda4307c4eb1ba300cb428ec5e
aminpourtavanaie <amin.pt.2003@gmail.con>
2026-01-01 14:32:40 +0330

    Fix bottleneck

--- ProfilingTest/src/JavaCup.java
+++ ProfilingTest/src/JavaCup.java
@@ -23,12 +23,10 @@ public class JavaCup {
         else { System.out.println("NO"); }
     }
     public static void temp() {
-        ArrayList a = new ArrayList();
         for (int i = 0; i < 10000; i++)
         {
             for (int j = 0; j < 20000; j++) {
-                a.add(i + j);
-            }
+                int calculation = i + j;             }
         }
     }
 }
